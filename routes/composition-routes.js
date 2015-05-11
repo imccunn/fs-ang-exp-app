2b918196ca350dd8e7a2e38c87e8ead1c04afb29
Ian McCunn <ianmccunn@gmail.com>
2015-05-10 19:36:22 -0700

    styling

--- routes/composition-routes.js
+++ routes/composition-routes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
 
   app.use(bodyparser.json());
 
-  app.get('/', function(req, res) {
+  app.get('/', function (req, res) {
     res.sendfile('../build/index.html');
   });
 
