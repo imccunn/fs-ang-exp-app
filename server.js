c4946d1142bfdedaecb1c466660cb445d98f4cad
Ian McCunn <ianmccunn@gmail.com>
2016-07-28 23:00:17 -0700

    lint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var express = require('express'),
-	mongoose = require('mongoose'),
-	compositionsRoutes = require('./routes/composition-routes'),
-	app,
-	mainRouter;
+	  mongoose = require('mongoose'),
+	  compositionsRoutes = require('./routes/composition-routes'),
+	  app,
+	  mainRouter;
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/compositionapp_dev');
 
@@ -12,11 +12,10 @@ app = express();
 app.use(express.static(__dirname + '/build'));
 
 mainRouter = express.Router();
-
 compositionsRoutes(mainRouter);
 
 app.use('/api/v1', mainRouter);
 
-app.listen(process.env.PORT || 3333, function () {
-  console.log('Server listening on port ' + (process.env.PORT || 3333));
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Server listening on port ' + (process.env.PORT || 8080));
 });
